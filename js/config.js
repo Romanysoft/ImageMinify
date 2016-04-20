ee157ca9b53a1c02f4c4a02389efad6838d012ae
iansun <lauer3912@gmail.com>
2016-04-20 16:27:38 +0900

    上传网站

---
+++ js/config.js
@@ -0,0 +1,15 @@
+(function () {
+    window.RTYConfig = {
+        appName:"ImageMinify",
+        appID:"com.romanysoft.app.macos.ImageMinify",
+        documentTitle:"ImageMinify By Romanysoft",
+        supportPlatforms:["Mac"],
+        googleUA:"UA-76676167-8",
+        gitHome:"//github.com/Romanysoft/ImageMinify",
+        reportIssueUrl:"//github.com/Romanysoft/ImageMinify/issues",
+        changeLogUrl:"//github.com/Romanysoft/ImageMinify/wiki/Changelog",
+        wikiUrl:"//github.com/Romanysoft/ImageMinify/wiki",
+        romanysoftHome:"//www.romanysoft.com",
+        copyright:"Copyright © " + (new Date()).getFullYear() + " Romanysoft LAB."
+    }
+})();
